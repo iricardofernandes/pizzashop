72ee7d8a8f0602375f7cb302ebfb2c38fa2f9295
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-01-10 22:31:23 -0300

    feat: add get order details route

---
+++ api/src/http/routes/get-order-details.ts
@@ -0,0 +1,62 @@
+import { db } from '@/db/connection'
+import { Elysia, NotFoundError, t } from 'elysia'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+
+export const getOrderDetails = new Elysia().use(auth).get(
+  '/orders/:orderId',
+  async ({ getCurrentUser, params }) => {
+    const { orderId } = params
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const order = await db.query.orders.findFirst({
+      columns: {
+        id: true,
+        status: true,
+        totalInCents: true,
+        createdAt: true,
+      },
+      with: {
+        customer: {
+          columns: {
+            name: true,
+            email: true,
+            phone: true,
+          },
+        },
+        orderItems: {
+          columns: {
+            id: true,
+            priceInCents: true,
+            quantity: true,
+          },
+          with: {
+            product: {
+              columns: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+      where(fields, { eq }) {
+        return eq(fields.id, orderId)
+      },
+    })
+
+    if (!order) {
+      throw new NotFoundError('Order not found')
+    }
+
+    return order
+  },
+  {
+    params: t.Object({
+      orderId: t.String(),
+    }),
+  }
+)
--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,6 +1,7 @@
 import { Elysia } from 'elysia'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getOrderDetails } from './routes/get-order-details'
 import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sentAuthLink } from './routes/send-auth-link'
@@ -13,6 +14,7 @@ const app = new Elysia()
   .use(signOut)
   .use(getProfile)
   .use(getManagedRestaurant)
+  .use(getOrderDetails)
 
 app.listen(3333, () => {
   console.log('HTTP server is running!')
