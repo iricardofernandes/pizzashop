9631a5b61a845384c55c2f2570cd0e7c8af3d99c
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-01-14 07:53:46 -0300

    feat: add react helmet (SEO)

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from 'react-router'
 import './index.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
