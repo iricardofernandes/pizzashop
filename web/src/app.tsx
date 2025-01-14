81aab8daf489110c677a183de6f4094e15c6cf7c
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-01-13 22:40:32 -0300

    style: add tailwind config

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button'
+import './index.css'
+
 export function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="w-screen h-screen flex justify-between items-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
