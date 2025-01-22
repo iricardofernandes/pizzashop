c38b5c921308e3acc13231a71c06a1d5dc58fa01
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-01-21 23:23:58 -0300

    feat: add theme toggle and profile menu

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from 'react-router'
 import './index.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
