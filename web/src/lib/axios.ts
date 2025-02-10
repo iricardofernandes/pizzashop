daf567651658a00d969e707bb588a72ddf6cc03d
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-02-09 23:02:52 -0300

    test: add sign in e2e test

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -6,7 +6,7 @@ export const api = axios.create({
   withCredentials: true,
 })
 
-if (env.VITE_API_URL) {
+if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async config => {
     await new Promise(resolve => setTimeout(resolve, Math.random() * 3000))
 
---
+++ web/test/sigin-in.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page
+    .getByRole('textbox', { name: 'Seu e-mail' })
+    .fill('johndoe@example.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Enviamos um e-mail de login!')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page
+    .getByRole('textbox', { name: 'Seu e-mail' })
+    .fill('wrongemail@example.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
